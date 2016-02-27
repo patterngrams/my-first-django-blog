b8cd4d00c9e8bf2b14d801976fd34dc0bbd5314a
patterngrams <christine.ernest@gmail.com>
2016-02-27 17:48:36 -0500

    Added custom X-human header.

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,9 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    response = render(request, 'blog/post_detail.html', {'post': post})
+    response['X-human'] = "Bow down to all my new Django knowledge ya'll"
+    return response
 
 
 def post_new(request):
